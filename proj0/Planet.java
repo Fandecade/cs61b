bab267d3e1d0f271fda1ee91c2d79f562265fa19
Fandecade <853663630@qq.com>
2024-09-17 21:33:29 +0800

    done the project0

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -2,12 +2,12 @@
 public class Planet {
 
     private static final double G =  6.67e-11;
-    double xxPos;
-    double yyPos;
-    double xxVel;
-    double yyVel;
-    double mass;
-    String imgFileName;
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
     
     // the constructor of empty planet
     public Planet(double xP, double yP, double xV, double yV, double m, String img) {
