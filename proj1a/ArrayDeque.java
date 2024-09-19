4de105e751e76029d4e92bd82466fd98d0833cab
Fandecade <853663630@qq.com>
2024-09-19 20:10:42 +0800

    finish the project1a

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,97 @@
+public class ArrayDeque<T> {
+    private T[] array;
+    private int size;
+    private int length;
+    private int front;
+    private int rear;
+
+    public ArrayDeque() {
+        array = (T[]) new Object[8];
+        front = 0;
+        rear = 0;
+        size = 0;
+        length = 8;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void resize(int newSize) {
+        T[] newArray = (T[]) new Object[newSize];
+        System.arraycopy(array, 0, newArray, 0, front);
+        array = newArray;
+    }
+
+    public void addFirst(T item) {
+        if (size == array.length) { //数组满时，扩充数组
+            resize(array.length * 2);
+        }
+        array[front] = item;
+        front = (front + 1) % array.length;
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        if (size == array.length) {
+            resize(array.length * 2);
+        }
+        array[rear] = item;
+        rear = (rear + 1) % array.length;
+        size += 1;
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        T item = array[front];
+        front = (front + 1) % array.length;
+        size -= 1;
+        return item;
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        T item = array[rear];
+        rear = (rear + 1) % array.length;
+        size -= 1;
+        return item;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return array[(front + index) % array.length];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        if(size == 0) {
+            return;
+        }
+        while(front < rear) {
+            System.out.print(array[front] + " ");
+            front = (front + 1) % array.length;
+        }
+        System.out.println();
+    }
+
+    public ArrayDeque(ArrayDeque<T> other) {
+        array = (T[]) new Object[8];
+        front = 0;
+        rear = 0;
+        size = other.size;
+        length = other.size;
+        while(front < rear) {
+            array[front] = other.array[front];
+            front = (front + 1) % array.length;
+        }
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,126 @@
+public class LinkedListDeque<T> {
+    public class node {    //创建双链表node结点
+        public node pre;
+        public T item;
+        public node next;
+
+        public  node(node pre, T item, node next) {  //node构造函数
+            this.pre = pre;
+            this.item = item;
+            this.next = next;
+        }
+
+        public  node(node pre, node next) {   //创建头结点
+            this.pre = pre;
+            this.next = next;
+        }
+    }
+    private node dummy; //头结点
+    private int size = 0;   //记录双端队列大小
+
+    public LinkedListDeque() {  //创建一个空链表队列（头结点的pre和next都指向自己）
+        dummy = new node(null,null);    //初始化头结点
+        dummy.next = dummy;
+        dummy.pre = dummy;
+        size = 0;   //初始化大小为0
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addFirst(T item) {
+        if(this.isEmpty()) {
+            node s = new node(dummy, item, dummy);
+            dummy.next = s;
+            dummy.pre = s;
+        }else{
+            node s = new node(dummy, item, dummy.next);
+            dummy.next.pre = s;
+            dummy.next = s;
+        }
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        if(this.isEmpty()) {
+            node s = new node(dummy, item, dummy);
+            dummy.next = s;
+            dummy.pre = s;
+        }else {
+            node s = new node(dummy.pre, item, dummy);
+            dummy.pre.next = s;
+            dummy.pre = s;
+        }
+        size += 1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        if(isEmpty()) {
+            return ;
+        }
+
+        node p = dummy.next;
+        while(p != dummy) {
+            System.out.println(p.item);
+            p = p.next;
+        }
+        System.out.print("");
+    }
+
+    public T removeFirst(){
+        if(isEmpty()) {
+            return null;
+        }
+        node p =dummy.next; //待删元素
+        dummy.next = p.next;
+        p.next.pre = dummy;
+        size -= 1;
+        if(size == 0){
+            dummy.pre = dummy;
+        }
+        return p.item;
+    }
+
+    public T removeLast() {
+        if(isEmpty()) {
+            return null;
+        }
+        node p = dummy.pre; //最后一个元素
+        p.pre.next = dummy;
+        dummy.pre = p.pre;
+        size -= 1;
+        if(size == 0){
+            dummy.next = dummy;
+        }
+        return p.item;
+    }
+
+    public T get(int index) {
+        if(isEmpty()) {
+            return null;
+        }
+        int i = 0;
+        node p = dummy.next;
+        while(i != index && p != dummy){
+            p = p.next;
+        }
+        return p == dummy ? null : p.item;
+    }
+
+    public LinkedListDeque(LinkedListDeque other) {
+        dummy =new node(null, null);
+        dummy.next = dummy;
+        dummy.pre = dummy;
+        size = 0;
+        for(int i = 0; i < other.size(); i++) {
+            addLast((T)other.get(i));
+        }
+    }
+
+
+}
